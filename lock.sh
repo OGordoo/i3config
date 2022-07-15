44488235d7f77ce82f309dd905e500ef66d115a0
OGordoo <marcosafonso8@hotmail.com>
2022-07-15 14:16:27 +0100

    removing old files

--- lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ICON=$HOME/Pictures/padlock.png
-TMPBG=/tmp/screen.png
-scrot /tmp/screen.png
-convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
-i3lock -u -i $TMPBG
-rm /tmp/screen.png
